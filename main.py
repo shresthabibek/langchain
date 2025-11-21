98cfb0c14e49605ceceb5cfff26fec43dc3f72a3
Bibek Shrestha <bibekshrestha101@gmail.com>
2025-11-21 00:07:16 -0600

    environment setup

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+from dotenv import load_dotenv
+
+import os
+
+load_dotenv()
+
 def main():
     print("Hello from pythonproject2!")
+    print(os.getenv("OPENAI_API_KEY"))
 
 
 if __name__ == "__main__":
