702833b32c3225c2b6e50adb21d107473a8313c3
Bibek Shrestha <bibekshrestha101@gmail.com>
2025-11-27 23:56:38 -0600

    created react agent

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from click import prompt
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,8 +10,25 @@ from langchain_tavily import TavilySearch
 
 
 
+tools = [TavilySearch()]
+llm = ChatOpenAI(model = "gpt-4")
+react_prompt = hub.pull("hwchase17/react")
+
+agent = create_react_agent(llm=llm,
+                           tools = tools,
+                           prompt = react_prompt
+                           )
+
+agent_executor = AgentExecutor(agent = agent, tools  = tools, verbose=True)
+chain = agent_executor
+
 def main():
-    print("Hello from pythonproject2!")
+    result = chain.invoke(
+        input={
+            "input":"search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details"
+        }
+    )
+    print(result)
 
 
 if __name__ == "__main__":
