637b5c3ea8c6640e53765029bf816ebc2411d500
Bibek Shrestha <bibekshrestha101@gmail.com>
2025-11-27 22:21:09 -0600

    added dependencies

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from langchain_classic import hub
+from langchain_classic.agents import AgentExecutor, create_react_agent
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+
+
+
 def main():
     print("Hello from pythonproject2!")
 
