e769bfc3f131d0622d8a7b56785e68b34f81282d
Bibek Shrestha <bibekshrestha101@gmail.com>
2025-12-02 23:27:54 -0600

    env setup

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from pythonproject2!")
+from dotenv import load_dotenv
 
+load_dotenv()
+#
+# def main():
+#     print("Hello from pythonproject2!")
+
+def get_text_length(text:str) -> int:
+    """Returns the length of a text by characters"""
+    return len(text)
 
 if __name__ == "__main__":
-    main()
+
+    print(get_text_length(text='Dogesh'))
