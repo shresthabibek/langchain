bd3f9f2cbf35679f5a61c37a6cb216ab163a572b
Bibek Shrestha <bibekshrestha101@gmail.com>
2025-12-11 16:20:58 -0600

    rag implemented

---
+++ ingestion.py
@@ -0,0 +1,29 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+
+
+
+if __name__ == "__main__":
+    print("Ingesting...")
+    loader = TextLoader("/Users/bibek/PycharmProjects/PythonProject2/mediumblog1.txt")
+    document = loader.load()
+
+    print("Splitting")
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    texts = text_splitter.split_documents(document)
+    print(f"created {len(texts)} chunks")
+
+    embeddings = OpenAIEmbeddings(openai_api_key=os.environ.get("OPENAI_API_KEY"))
+
+    print("ingesting...")
+    PineconeVectorStore.from_documents(
+        texts, embeddings, index_name=os.environ["INDEX_NAME"]
+    )
+    print("finish")
